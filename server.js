d9e01e5fe47b9122b31eee005baadcfd1a6b340b
saad-mamun <mamunali88991@gmail.com>
2025-12-29 23:32:07 +0600

    simplerouting

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 import http from "http";
-const PORT = 7000
-const server = http.createServer((req, res) => {
+import fs from "fs/promises"
+import path from "path";
+import url from "url";
+const PORT = process.env.PORT;
 
-    res.setHeader("Content-Type", "text/html")
-    res.write("<h1>Hello World!</h1>");
-    res.end()
-})
+
+// Get Current Path
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname, __filename)
+
+const server = http.createServer((req, res) => {
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Hello World!</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About Page!</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method Not Allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("500 Internal Server Error");
+  }
+});
 
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
