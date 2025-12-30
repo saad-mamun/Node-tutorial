da413cbfb7f304225e3e03685b7d55c6ccc07087
saad-mamun <mamunali88991@gmail.com>
2025-12-30 23:17:36 +0600

    Simple api building

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
 import http from "http";
-import fs from "fs/promises"
+import fs from "fs/promises";
 import path from "path";
 import url from "url";
 const PORT = process.env.PORT;
-
 
 // Get Current Path
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(__dirname, __filename)
+console.log(__dirname, __filename);
 
-const server = http.createServer( async (req, res) => {
+const server = http.createServer(async (req, res) => {
   try {
     // Check if get request
     if (req.method === "GET") {
-        let filePath;
+      let filePath;
 
       if (req.url === "/") {
-        filePath = path.join(__dirname, "public", "index.html")
+        filePath = path.join(__dirname, "public", "index.html");
       } else if (req.url === "/about") {
-       filePath = path.join(__dirname, "public", "about.html");
+        filePath = path.join(__dirname, "public", "about.html");
       } else {
         throw new Error("Not Found");
       }
 
-const data = await fs.readFile(filePath);
-res.setHeader("Content-Type", "text/html");
-res.write(data);
-res.end();
-
-
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
     } else {
       throw new Error("Method Not Allowed");
     }
---
+++ server2.js
@@ -0,0 +1,39 @@
+import { createServer } from "http";
+const PORT = process.env.PORT;
+
+const users = [
+  { id: 1, name: "Liton" },
+  { id: 2, name: "Mamun" },
+  { id: 3, name: "Milon" },
+  { id: 4, name: "Sohidul" },
+];
+
+
+
+// Simple api building
+const server = createServer((req, res) => {
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(users));
+    res.end();
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+    const id = req.url.split("/")[3];
+    const user = users.find((u) => u.id === parseInt(id));
+    if (user) {
+      res.write(JSON.stringify(user));
+    } else {
+      res.statusCode = 404;
+      res.write(JSON.stringify({ message: "User not found" }));
+    }
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
+    res.write(JSON.stringify({ message: "Route not found" }));
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
